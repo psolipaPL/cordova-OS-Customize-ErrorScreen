42c9327d6362038f7e30115c41d88a1459a1a85c
Paulo Solipa <psolipa@product-league.com>
2024-10-12 17:28:43 +0100

    zdsfg

--- hooks/utils.js
+++ hooks/utils.js
@@ -39,7 +39,7 @@ const replaceHTMLContent = (dir, fileName, targetFilePath, selector) => {
         console.log(`Source File: ${sourceFile}`)
         const targetFile = parseHTML(readFile(targetFilePath));
         console.log(`target File: ${targetFile}`)
-        //targetFile.querySelector(selector).toString() === sourceFile.querySelector(selector).toString()
+        targetFile.querySelector(selector).toString() = sourceFile.querySelector(selector).toString()
         console.log(`Target file to string: ${targetFile.querySelector(selector).toString()}`)
         console.log(`Source file to string: ${sourceFile.querySelector(selector).toString()}`)
         fs.writeFileSync(targetFilePath, targetFile);
