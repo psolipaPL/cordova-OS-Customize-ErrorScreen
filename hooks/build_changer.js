1ae8534ea8d1661e2998544218917fbae413311e
Paulo Solipa <psolipa@product-league.com>
2024-10-13 13:39:04 +0100

    test

--- hooks/build_changer.js
+++ hooks/build_changer.js
@@ -4,7 +4,5 @@ module.exports = function (context) {
     const confs = utils.getConfigs();
 
     //Error File Changer
-    //let errorFileContent = utils.readFile(context.opts.projectRoot + confs.androidPath + confs.errorFile);
-    //utils.errorFileReplacer(context.opts.projectRoot + confs.androidPath + confs.errorFile, errorFileContent, confs.textToReplace, '');
     utils.replaceHTMLContent(context.opts.projectRoot + confs.androidPath,'customError' , context.opts.projectRoot + confs.androidPath + confs.errorFile, '#error-screen-wrapper')
 }
